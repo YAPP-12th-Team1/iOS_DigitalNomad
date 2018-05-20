8eacd4f8b11fbc7bce5008d63745e1244181a1fc
YangJongYeol <yajya004@gmail.com>
2018-05-20 21:43:57 +0900

    Insert logic 1. Overall logic of PlaceViewController 2. Update my current location in MyDetailViewController & Firebase

--- DigitalNomad/Map/PlaceViewController.swift
+++ DigitalNomad/Map/PlaceViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 import DZNEmptyDataSet
+import RealmSwift
 import Alamofire
 import SwiftyJSON
 import MapKit
 import CoreLocation
+import Firebase
+import FirebaseStorage
 import SnapKit
 
 class PlaceViewController: UIViewController {
-
+    var filteredData: [String]!
+    var loctaion_name_array = [String]()
+    
+    var placeNameArr = [String]()
+    var addressNameArr = [String]()
+    var distance = [String]()
+    var xArr = [String]()
+    var yArr = [String]()
+    var categoryNameArr = [String]()
+    
+    let realm = try! Realm()
+    
+    let locationManager = CLLocationManager()
+    var myLat : Double = 0.0
+    var myLong : Double = 0.0
+    
+    var mapView: MTMapView!
+    
+    var flag : Int = 0 // 0,3 : 더 보기(거리순), 1,4 : 더 보기(최신순) ,2 : 지도 보기
+    var isFirst : Bool = true
+    var isFirstExecuted: Bool = false
     
     //MARK:- IBOutlets
     @IBOutlet var upperView: UIImageView!
@@ -43,8 +66,10 @@ class PlaceViewController: UIViewController {
         self.toggleButton.addTarget(self, action: #selector(touchUpToggleButton(_:)), for: .touchUpInside)
         self.recentCheckButton.addTarget(self, action: #selector(touchUpRecentCheckButton(_:)), for: .touchUpInside)
         self.distanceCheckButton.addTarget(self, action: #selector(touchUpDistanceCheckButton(_:)), for: .touchUpInside)
+        
+        initMapView()
     }
-
+    
     //MARK:- 커스텀 메소드
     //MARK: 토글 버튼 액션 메소드
     @objc func touchUpToggleButton(_ sender: UIButton) {
@@ -78,6 +103,8 @@ class PlaceViewController: UIViewController {
         if sender.isSelected { return }
         sender.isSelected = !sender.isSelected
         self.distanceCheckButton.isSelected = !sender.isSelected
+        self.flag = 3
+        self.tableView.reloadData()
     }
     
     //MARK: 거리순 라디오 버튼 액션 메소드
@@ -85,6 +112,8 @@ class PlaceViewController: UIViewController {
         if sender.isSelected { return }
         sender.isSelected = !sender.isSelected
         self.recentCheckButton.isSelected = !sender.isSelected
+        self.flag = 4
+        self.tableView.reloadData()
     }
     
     //MARK: 라디오 버튼 생성
@@ -123,6 +152,69 @@ class PlaceViewController: UIViewController {
 extension PlaceViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //검색 로직
+        
+        self.placeNameArr.removeAll()
+        self.addressNameArr.removeAll()
+        self.distance.removeAll()
+        self.xArr.removeAll()
+        self.yArr.removeAll()
+        self.categoryNameArr.removeAll()
+        
+        print("search : ", textField.text!)
+        
+        
+        let baseUrl: String = "https://dapi.kakao.com/v2/local/search/keyword.json?query="
+        let query: String = textField.text!
+        let encode = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let modi = encode?.replacingOccurrences(of: "%25", with: "%")
+        
+        Alamofire.request(baseUrl+modi!+"&radius=20000&x=\(myLong)&y=\(myLat)", method: .get ,headers: ["Authorization": "KakaoAK 8add144f51d5e214bb8d9008445c817d"])
+            .responseJSON { response in
+                // check for errors
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error calling GET on /todos/1")
+                    print(response.result.error!)
+                    return
+                }
+                
+                // make sure we got some JSON since that's what we expect
+                guard let json = response.result.value as? [String: Any] else {
+                    print("didn't get todo object as JSON from API")
+                    print("Error: \(String(describing: response.result.error))")
+                    return
+                }
+                
+                // get and print the title
+                guard let searchedPlace = json["documents"] as? [[String: Any]] else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+                
+                for placeIndex in searchedPlace {
+                    self.placeNameArr.append(placeIndex["place_name"] as! String)
+                    self.addressNameArr.append(placeIndex["address_name"] as! String)
+                    self.distance.append(placeIndex["distance"] as! String)
+                    self.xArr.append(placeIndex["x"] as! String)
+                    self.yArr.append(placeIndex["y"] as! String)
+                    self.categoryNameArr.append(placeIndex["category_group_name"] as! String)
+                }
+                
+                var items = [MTMapPOIItem]()
+                
+                for index in 0..<self.placeNameArr.count {
+                    items.append(self.poiItem(name: self.placeNameArr[index], address: self.addressNameArr[index], distance: "\(self.distance(lat1: self.myLat, lng1: self.myLong, lat2: Double(self.yArr[index])!, lng2: Double(self.xArr[index])!))" , latitude: self.yArr[index], longitude: self.xArr[index], category: self.categoryNameArr[index]))
+                }
+                
+                self.mapView.removeAllPOIItems()
+                self.mapView?.addPOIItems(items)
+                
+                print(self.placeNameArr)
+                print(self.categoryNameArr)
+                
+                self.mapView?.fitAreaToShowAllPOIItems()
+        }
+        
         textField.endEditing(true)
         return true
     }
@@ -130,17 +222,220 @@ extension PlaceViewController: UITextFieldDelegate {
 
 extension PlaceViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        myLat = locations[locations.count-1].coordinate.latitude
+        myLong = locations[locations.count-1].coordinate.longitude
+        print(myLat)
         
+        // userInfo 갱신 부분
+        let baseUrl: String = "https://dapi.kakao.com/v2/local/geo/coord2address.json?"
+        Alamofire.request(baseUrl+"x=\(myLong)&y=\(myLat)", method: .get ,headers: ["Authorization": "KakaoAK 8add144f51d5e214bb8d9008445c817d"]).responseJSON {
+            response in
+            guard response.result.error == nil else {
+                print("error calling GET on /todos/1")
+                print(response.result.error!)
+                return
+            }
+            guard let json = response.result.value as? [String: Any] else {
+                print("didn't get todo objecy as JSON from API")
+                print("Error: \(String(describing: response.result.error))")
+                return
+            }
+            guard let doc = json["documents"] as? [[String: Any]] else {
+                print("Could not get documents from JSON")
+                return
+            }
+            let addr = doc[0]["address"] as! [String:String]
+            let region1 = addr["region_1depth_name"]!
+            let region2 = addr["region_2depth_name"]!
+            let str = region1+" "+region2
+            
+            var userInfo : UserInfo!
+            userInfo = self.realm.objects(UserInfo.self).last
+            
+            try! self.realm.write{
+                userInfo.address = str
+            }
+            Database.database().reference().child("users").child((Auth.auth().currentUser?.uid)!).updateChildValues([
+                "address" : str
+                ])
+        }
+        //
+        
+        if isFirst {
+            isFirst = false
+            let item = MTMapPOIItem()
+            item.tag = -1
+            item.itemName = "현 위치"
+            item.markerType = .bluePin
+            item.markerSelectedType = .bluePin
+            item.mapPoint = MTMapPoint(geoCoord: .init(latitude: myLat, longitude: myLong))
+            item.showAnimationType = .noAnimation
+            item.customImageAnchorPointOffset = .init(offsetX: 30, offsetY: 0)    // 마커 위치 조정
+            self.mapView?.remove(self.mapView.findPOIItem(byTag: -1))
+            self.mapView?.add(item)
+            
+            let mapPoint = MTMapPoint.init(geoCoord: MTMapPointGeo.init(latitude: myLat, longitude: myLong))
+            
+            self.mapView?.setMapCenter(mapPoint, animated: true)
+            self.mapView?.setZoomLevel(3, animated: true)
+        }
+        
+        //distance 배열을 갱신하자.
+        let obj = self.realm.objects(MapLocationInfo.self)
+        for index in 0..<obj.count {
+            try! realm.write {
+                obj[index].distance = "\(distance(lat1: myLat, lng1: myLong, lat2: obj[index].latitude, lng2: obj[index].longitude))"
+            }
+        }
     }
 }
 
 //MARK:- 맵뷰 델리게이트 구현
 extension PlaceViewController: MTMapViewDelegate {
+    func initMapView() {
+        // 위치 사용 동의 알람창 최초
+        isAuthorizedtoGetUserLocation()
+        
+        // 위치 동의 완료 위치 정보 사용
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation() // 위치 정보 받음
+        }
+        
+        self.filteredData = self.loctaion_name_array
+        
+        mapView = MTMapView(frame: CGRect(x: 0, y: 0, width: self.centerView.frame.size.width, height: self.centerView.frame.size.height))
+        
+        //mapView.daumMapApiKey = "YOUR_DAUM_API_KEY"
+        self.mapView!.delegate = self
+        self.mapView!.baseMapType = .standard
+        self.centerView.addSubview(self.mapView!)
+        self.loadStoredItems()
+    }
+    
+    func poiItem(name: String, address: String, distance: String, latitude: String, longitude: String, category: String) -> MTMapPOIItem {
+        let item = MTMapPOIItem()
+        item.itemName = name
+        item.markerType = .redPin
+        item.markerSelectedType = .redPin
+        item.mapPoint = MTMapPoint(geoCoord: .init(latitude: Double(latitude)!, longitude: Double(longitude)!))
+        item.showAnimationType = .noAnimation
+        item.customImageAnchorPointOffset = .init(offsetX: 30, offsetY: 0)    // 마커 위치 조정
+        
+        return item
+    }
+    
+    func storedPoiItem(name: String, address: String, distance: String, latitude: String, longitude: String, category: String) -> MTMapPOIItem {
+        let item = MTMapPOIItem()
+        item.itemName = name
+        item.markerType = .yellowPin
+        item.markerSelectedType = .bluePin
+        item.mapPoint = MTMapPoint(geoCoord: .init(latitude: Double(latitude)!, longitude: Double(longitude)!))
+        item.showAnimationType = .noAnimation
+        item.customImageAnchorPointOffset = .init(offsetX: 30, offsetY: 0)    // 마커 위치 조정
+        
+        return item
+    }
+    
+    // 구 삼각법을 기준으로 대원거리(m단위) 요청
+    func distance(lat1: Double, lng1: Double, lat2: Double, lng2: Double) -> Double {
+        
+        // 위도,경도를 라디안으로 변환
+        let rlat1 = lat1 * .pi / 180
+        let rlng1 = lng1 * .pi / 180
+        let rlat2 = lat2 * .pi / 180
+        let rlng2 = lng2 * .pi / 180
+        
+        // 2점의 중심각(라디안) 요청
+        let a =
+            sin(rlat1) * sin(rlat2) +
+                cos(rlat1) * cos(rlat2) *
+                cos(rlng1 - rlng2)
+        let rr = acos(a)
+        
+        // 지구 적도 반경(m단위)
+        let earth_radius = 6378140.0
+        
+        // 두 점 사이의 거리 (m단위)
+        let distance = earth_radius * rr
+        
+        return distance/1000
+    }
+    
+    func loadStoredItems() -> Void {
+        var str: String
+        var items = [MTMapPOIItem]()
+        let obj = self.realm.objects(MapLocationInfo.self)
+        for index in 0..<obj.count {
+            switch(obj[index].category) {
+            case 0:
+                str = "대형마트"
+            case 1:
+                str = "편의점"
+            case 2:
+                str = "어린이집, 유치원"
+            case 3:
+                str = "학교"
+            case 4:
+                str = "학원"
+            case 5:
+                str = "주차장"
+            case 6:
+                str = "주유소, 충전소"
+            case 7:
+                str = "지하철역"
+            case 8:
+                str = "은행"
+            case 9:
+                str = "문화시설"
+            case 10:
+                str = "중개업소"
+            case 11:
+                str = "공공기관"
+            case 12:
+                str = "관광명소"
+            case 13:
+                str = "숙박"
+            case 14:
+                str = "음식점"
+            case 15:
+                str = "카페"
+            case 16:
+                str = "병원"
+            case 17:
+                str = "약국"
+            default:
+                str = "Error"
+            }
+            
+            items.append(storedPoiItem(name: obj[index].name, address: obj[index].address, distance: "\(self.distance(lat1: self.myLat, lng1: self.myLong, lat2: obj[index].latitude, lng2: obj[index].longitude))", latitude: String(obj[index].latitude), longitude: String(obj[index].longitude), category: str))
+        }
+        
+        self.mapView!.addPOIItems(items)
+        self.mapView?.fitAreaToShowAllPOIItems()
+        self.view.addSubview(self.tableView!)
+    }
+    
+    func isAuthorizedtoGetUserLocation() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
     func mapView(_ mapView: MTMapView!, selectedPOIItem poiItem: MTMapPOIItem!) -> Bool {
         if poiItem.markerType == .yellowPin { return true }
         let alert = UIAlertController(title: nil, message: "마커를 추가할까요?", preferredStyle: .alert)
         let yesAction = UIAlertAction(title: "예", style: .default) { (action) in
-            
+            for index in 0..<self.placeNameArr.count {
+                if(self.placeNameArr[index] == poiItem.itemName) {
+                    addMapLocation(Double(self.xArr[index])!, Double(self.yArr[index])!, getCategory(self.categoryNameArr[index]), self.placeNameArr[index], self.addressNameArr[index], "\(self.distance(lat1: self.myLat, lng1: self.myLong, lat2: Double(self.xArr[index])!, lng2: Double(self.yArr[index])!))", Date())
+                    self.mapView?.removeAllPOIItems()
+                    self.loadStoredItems()
+                    self.tableView.reloadData()
+                    break;
+                }
+            }
         }
         let noAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
         alert.addAction(yesAction)
@@ -152,19 +447,69 @@ extension PlaceViewController: MTMapViewDelegate {
 
 //MARK:- 테이블 뷰 데이터 소스 구현
 extension PlaceViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let obj = self.realm.objects(MapLocationInfo.self)
+        return obj.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "mapCell", for: indexPath) as? PlaceCell else { return UITableViewCell() }
         
+        var obj = self.realm.objects(MapLocationInfo.self)
+        if flag == 1 || flag == 4 || flag == 5 { obj = obj.sorted(byKeyPath: "distance", ascending: true) }
+        else if flag == 0 || flag == 3 { obj = obj.sorted(byKeyPath: "update", ascending: false) }
+        let placeName = obj[indexPath.row].name
+        let placeAddress = obj[indexPath.row].address
+        let placeCategory = obj[indexPath.row].category
+        let curDistance = self.distance(lat1: self.myLat, lng1: self.myLong, lat2: obj[indexPath.row].latitude, lng2: obj[indexPath.row].longitude)
+    
+        cell.titleLabel?.text = placeName
+        cell.addressLabel?.text = placeAddress
+        cell.distanceLabel?.text = "\(round(100*curDistance)/100)km"
+        
+        switch placeCategory {
+        case 15:
+            cell.placeImageView.image = #imageLiteral(resourceName: "cafe.png")
+        case 14:
+            cell.placeImageView.image = #imageLiteral(resourceName: "restaurant.png")
+        default:
+            cell.placeImageView.image = #imageLiteral(resourceName: "kamel.png")
+        }
+        
         return cell
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
     }
 }
 
 //MARK:- 테이블 뷰 델리게이트 구현
 extension PlaceViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var obj = self.realm.objects(MapLocationInfo.self)
+        if flag == 1 || flag == 4 || flag == 5 { obj = obj.sorted(byKeyPath: "distance", ascending: true) }
+        else if flag == 0 || flag == 3 { obj = obj.sorted(byKeyPath: "update", ascending: false) }
+        let objClicked = obj[indexPath.row]
+        let objClickedMapPoint = MTMapPoint.init(geoCoord: MTMapPointGeo.init(latitude: objClicked.latitude-0.0015, longitude: objClicked.longitude))
+        self.mapView?.setMapCenter(objClickedMapPoint, animated: true)
+        
+        self.mapView.removeAllPOIItems()
+        
+        let item = MTMapPOIItem()
+        item.itemName = objClicked.name
+        item.markerType = .yellowPin
+        item.markerSelectedType = .bluePin
+        item.mapPoint = objClickedMapPoint
+        item.showAnimationType = .noAnimation
+        item.customImageAnchorPointOffset = .init(offsetX: 30, offsetY: 0)    // 마커 위치 조정
+        
+        self.mapView.add(item)
+    }
 }
 
 extension PlaceViewController: DZNEmptyDataSetSource {
